b82b0d6dd9c29e472410b341d529fd45b86ee9d4
Ott, Jörn WI (Fa. Allgeier Experts / Goetzfried) <Joern.Ott@schufa.de>
2022-08-10 16:40:36 +0200

    Bump version to 1.0.2

--- _build/version.sh
+++ _build/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # see https://github.com/joernott/monitoring-check_log_elasticsearch/tags
 APP="monitoring-check_log_elasticsearch"
-VERSION="1.0.1"
+VERSION="1.0.2"
 RELEASE="1"
 URL="https://github.com/joernott/monitoring-check_log_elasticsearch/archive/refs/tags/v${VERSION}.tar.gz"
 ARCH="x86_64"
